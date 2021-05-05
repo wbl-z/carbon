ac8284820e676d77123423c915e2e46a7c9f27f3
201250139 <876865565@qq.com>
2021-05-05 16:28:56 +0800

    在正式开工前,大规模优化了一下代码结构和框架4.0

--- src/button.py
+++ src/button.py
@@ -2,8 +2,11 @@ import pygame
 
 
 class Button:
-    def __init__(self, screen, centerx, centery, width, height, button_color, txt_color, message, font, font_size):
+    def __init__(self, screen, centerX, centerY, width, height, button_color, txt_color, message, font, font_size):
         """初始化button"""
+        self.message_image_rect = self.message_image.get_rect()  # 获取参数
+        self.message_image = self.font.render(self.message, True,
+                                              self.txt_color)  #
         self.screen = screen  # 方便在后面的方法中用到
         self.width = width
         self.height = height
@@ -15,17 +18,14 @@ class Button:
 
         self.button_rect = pygame.Rect(0, 0, self.width, self.height)  # 创建按钮矩形
         # 调整位置
-        self.button_rect.centerx = centerx
-        self.button_rect.centery = centery
+        self.button_rect.centerx = centerX
+        self.button_rect.centery = centerY
 
         self.prepare_message()  # 放在__init__中，从而一开始就有
 
     def prepare_message(self):
         """把msg渲染成图像，并使其在按钮上居中"""
-        self.message_image = self.font.render(self.message, True,
-                                              self.txt_color)  #
         # 把字体文件渲染成图像，第一个参数是文本信息，第二个是是否开启防锯齿，即是否让边缘平滑，第三个是字体颜色，第四个是背景的颜色，这个背景是刚好把文字包括起来的一个矩形，不填则为透明背景
-        self.message_image_rect = self.message_image.get_rect()  # 获取参数
         self.message_image_rect.center = self.button_rect.center
 
     def draw_button(self):
--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,6 @@
-class Settings():
+class Settings:
     def __init__(self):
-        '''游戏中的参数设置'''
+        """游戏中的参数设置"""
         #屏幕参数
         # self.screen_width = 1200直接全屏
         # self.screen_height = 750
